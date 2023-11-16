cc107ae4b6b31b04e9262161dfc0490e3f096695
kayellas <zeynepmlfl@gmail.com>
2023-11-16 13:28:31 +0300

    login router tasarlandı

--- routers/routes.js
+++ routers/routes.js
@@ -1,3 +1,4 @@
+/*
 const router=require('express').Router()
 const {login,register}= require('../controllers/controller')
 router.post("/login",login)
@@ -12,4 +13,14 @@ patch: veri güncelleme
 delete: veri silme
 */
 
+// module.exports=router 
+
+
+const router=require('express').Router()
+const {login,register}=require('../controllers/controller')
+router.post("/login",login)
+
+router.post("/register",register)
+//post:veri gönderme get:veri alma put:veri güncelleme patch:veri güncelleme
+//delete:veri silme
 module.exports=router
