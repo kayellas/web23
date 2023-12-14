0f374287d954794d3c9fd806f7c2b2734a83f679
kayellas <zeynepmlfl@gmail.com>
2023-12-14 13:43:32 +0300

    veri_isleme

--- app.js
+++ app.js
@@ -23,17 +23,38 @@ app.use('/api',router)
 app.listen(process.env.PORT) 
 */
 
+
+
 const express = require('express')
 const db_connect=require('./db/mysql_connect')
 const router=require('./routers')
 require('dotenv/config')
 const app = express()
+/* 
 app.get('/', function (req, res) {
   res.send('Anasayfa')
 })
 app.get('/app', function (req, res) {
     res.send('app sayfası')
   })
+*/
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,"index.html"))
+})
+app.get('/musteri_ekle', function (req, res) {
+  res.sendFile(path.join(__dirname,"musteri_ekle.html"))
+})
+app.get('/musteri_sil', function (req, res) {
+  res.sendFile(path.join(__dirname,"musteri_sil.html"))
+})
+app.get('/musteri_guncelle', function (req, res) {
+  res.sendFile(path.join(__dirname,"musteri_guncelle.html"))
+})
+app.get('/musteri_getir', function (req, res) {
+  res.sendFile(path.join(__dirname,"musteri_getir.html"))
+})
+
 //Midllewares
 app.use(express.json({limit:'50mb',extended:true,parameterLimit:50000}))
 app.use('/api',router)
